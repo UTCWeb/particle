9e66ffc4b6458ce24942654d4b2bc7b3cbbdd92f
Bridget Hornsby <82905787+UTCbridget@users.noreply.github.com>
2021-08-17 13:31:23 -0400

    Update tailwind.config.js
    Add Tailwind configuration to match existing non-tailwind configuration. Also, FontAwesome css not being picked up in PL (including the CDN all.css file. Had manually add for PL). Add custom FontAwesome sizing to simulate existing configuration.

--- source/default/tailwind.config.js
+++ source/default/tailwind.config.js
@@ -11,6 +11,7 @@ const tailwindForms = require('@tailwindcss/forms');
 const colors = require('./tokens/colors.tailwind');
 const customForms = require('./tokens/forms.tailwind');
 const fontFamily = require('./tokens/font-family.tailwind');
+const { fontSize } = require('tailwindcss/defaultTheme');
 
 // Default Tailwind config can be found here: https://github.com/tailwindcss/tailwindcss/blob/v1.2.0/stubs/defaultConfig.stub.js
 module.exports = {
@@ -41,6 +42,22 @@ module.exports = {
         'utchero': '1fr 60% 35% 1fr',
         'utcheroright': '1fr 35% 60% 1fr',
         'utcherocenter': '1fr 45% 45% 1fr',
+      },
+      flex: {
+        'utccard': '0 0 auto',
+        'utccardbody': '1 0 65%',
+        'utccardimg': '0 1 35%',
+      },
+      boxShadow: {
+        'utccardnone': '0 0 4px 4px rgba(17,46,81,.25)',
+        'utccardwhite': '0 10px 15px -3px rgb(0 0 0 / 10%), 0 4px 6px -2px rgb(0 0 0 / 5%)',
+      },
+      fontSize: {
+        'icon-sm': '1rem',
+        'icon-md': '3rem',
+        'icon-lg': '5rem',
+        'icon-xl': '7rem',
+        'icon-2x': '9rem',
       }
     },
     minHeight: {
