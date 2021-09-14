71a7238d75f2f5eee7e05075ef2f1797f31449fe
Bridget Hornsby <82905787+UTCbridget@users.noreply.github.com>
2021-09-14 15:41:50 -0400

    UTC-325: Add drone shot to the select options.
    UTC-325: Add drone shot to the select options.

--- source/default/_patterns/00-protons/index.js
+++ source/default/_patterns/00-protons/index.js
@@ -2,8 +2,9 @@
  * Base css generation and global js logic.
  */
 
-import './tailwind.tokens.css';
+ import './tailwind.tokens.css';
 
+<<<<<<< Updated upstream
 //Legacy
 
 //Legacy CSS
@@ -55,3 +56,57 @@ export default {
     // Demo only, remove in practice
     GLOBAL_CONSTANT: 'particle',
 };
+=======
+ //Legacy
+ 
+ //Legacy CSS
+ import './legacy/css/footer.css';
+ import './legacy/css/global.css';
+ import './legacy/css/utc-sidebar-menu.css';
+ import './legacy/css/utc-offcanvas-sidebar-menu.css';
+ import './legacy/css/utc_department_info.css';
+ import './legacy/css/layout-builder.css';
+ import './legacy/css/off-canvas.css';
+ import './legacy/css/components/header/_utc_custom_header.css';
+ import './legacy/css/components/header/_main-navigation.css';
+ import './legacy/css/components/buttons/_buttons.css';
+ import './legacy/css/components/forms/_forms.css';
+ import './legacy/css/components/forms/_webforms.css';
+ import './legacy/css/components/section/_section.css';
+ import './legacy/css/components/UTC-custom-blocks/_utc_blockquotes.css';
+ import './legacy/css/components/UTC-custom-blocks/_utc-button-group.css';
+ import './legacy/css/ckeditor/accordion.css';
+ import './legacy/css/ckeditor/jquery-overflow.css';
+ import './legacy/css/components/UTC-custom-blocks/_utc_employee.css';
+ import './legacy/css/components/card/_card.css';
+ import './legacy/css/components/UTC-custom-blocks/_utclib_item_card.css';
+ import './legacy/css/components/UTC-custom-blocks/_utclib_primo_search.css';
+ import './legacy/css/components/UTC-custom-blocks/_utclib_help_btn.css';
+ import './legacy/css/components/UTC-custom-blocks/_utclib_events_feed.css';
+ import './legacy/css/components/UTC-custom-blocks/_utclib_guides.css';
+ import './legacy/css/components/UTC-custom-blocks/_utclib_events.css';
+ import './legacy/css/components/navigation/_breadcrumb.css';
+ import './legacy/css/components/navigation/_footer-menu.css';
+ import './legacy/css/components/UTC-custom-blocks/_utc_directory.css';
+ import './legacy/css/information-technology/_header.css';
+ import './legacy/css/information-technology/_midpagewidget.css';
+ import './legacy/css/digital_measures/tabbed.css';
+ import './legacy/css/pages/utc_homepages_options.css';
+ import './legacy/css/pages/utc_error_pages.css';
+ import './legacy/css/components/section/_utc_parallax.css';
+ // import "./legacy/css/components/UTC-custom-blocks/";
+ // import "./legacy/css/components/field/";
+ 
+ 
+ //Legacy JS
+ import './legacy/js/utc-sidebar-menu.js';
+ import './legacy/js/slick-custom-arrows.js';
+ // import './legacy/js/ckeditor-jquery.js';
+ 
+ // Export global variables.
+ export default {
+     // Demo only, remove in practice
+     GLOBAL_CONSTANT: 'particle',
+ };
+ 
+>>>>>>> Stashed changes
