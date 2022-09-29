b38afc5c9b5b34bb9cfb3e01ecaa71cab94c7db0
Bernardo Martinez <bernardo-martinez@utc.edu>
2022-09-29 19:06:09 -0400

    UTC-475: Updated postcss8 files.

--- source/default/webpack.config.js
+++ source/default/webpack.config.js
@@ -12,57 +12,57 @@ const namespaces = require('./namespaces');
 const { ASSETS_ATOMIC_FOLDER } = require('../../particle.root.config');
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'postcss-loader',
-            options: {
-              config: {
-                path: path.join('postcss.config.js'),
-                ctx: {
-                  // PostCSS Tailwind config
-                  tailwindConfig: path.resolve(__dirname, 'tailwind.config.js'),
-                },
-              },
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: [{
+                    loader: 'postcss-loader',
+                    options: {
+                        postcssOptions: {
+                            // config: {
+                            config: path.join('postcss.config.js'),
+                            // ctx: {
+                            //     // PostCSS Tailwind config
+                            //     tailwindConfig: path.resolve(__dirname, 'tailwind.config.js'),
+                            // },
+                            // },
+                        },
+                    },
+                }, ],
             },
-          },
+            {
+                test: /\.svg$/,
+                loader: 'file-loader',
+                options: {
+                    // postcssOptions: {
+                    name: '[path][name].[ext]',
+                    outputPath: ASSETS_ATOMIC_FOLDER,
+                    context: path.resolve(__dirname),
+                    emit: true,
+                    // },
+                },
+            },
         ],
-      },
-      {
-        test: /\.svg$/,
-        loader: 'file-loader',
-        options: {
-          name: '[path][name].[ext]',
-          outputPath: ASSETS_ATOMIC_FOLDER,
-          context: path.resolve(__dirname),
-          emit: true,
-        },
-      },
+    },
+    plugins: [
+        // Sprite system options
+        new SVGSpritemapPlugin(
+            path.resolve(namespaces.atoms, 'svg/icons/**/*.svg'), {
+                output: {
+                    filename: 'images/spritemap.svg',
+                    svg4everybody: true,
+                    svgo: true,
+                },
+                styles: {
+                    filename: '~svg-icons.css',
+                },
+            }
+        ),
     ],
-  },
-  plugins: [
-    // Sprite system options
-    new SVGSpritemapPlugin(
-      path.resolve(namespaces.atoms, 'svg/icons/**/*.svg'),
-      {
-        output: {
-          filename: 'images/spritemap.svg',
-          svg4everybody: true,
-          svgo: true,
-        },
-        styles: {
-          filename: '~svg-icons.css',
-        },
-      }
-    ),
-  ],
-  resolve: {
-    // JavaScript can import other components via shorthand, eg:
-    //   `import thing from 'atoms/thing';`
-    alias: namespaces,
-    extensions: ['.js', '.json'],
-  },
+    resolve: {
+        // JavaScript can import other components via shorthand, eg:
+        //   `import thing from 'atoms/thing';`
+        alias: namespaces,
+        extensions: ['.js', '.json'],
+    },
 };
--- webpack.particle.js
+++ webpack.particle.js
@@ -27,125 +27,121 @@ const { NODE_ENV = 'production' } = process.env;
 // process.traceDeprecation = true;
 
 module.exports = {
-  // entry: {}, // See entryPrepend() and particle() below for entry details
-  mode: NODE_ENV, // development|production
-  output: {
-    filename: '[name].js',
-  },
-  devtool: NODE_ENV === 'development' ? 'eval' : 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          loaders: {
-            js: 'babel-loader',
-          },
-        },
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-          {
-            loader: 'resolve-url-loader',
-            options: {
-              sourceMap: true,
-              root: '',
-            },
-          },
-          {
-            // PostCSS config at ./postcss.config.js
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true,
-              ident: 'postcss',
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(js|vue)$/,
-        enforce: 'pre',
-        //exclude: /node_modules/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
-        options: {
-          emitWarning: true, // development only
-          // emitWarning: false, // production only
-        },
-      },
-      {
-        test: /\.js$/,
-        // @babel runtime and core must NOT be transformed by babel
-        exclude: /@babel(?:\/|\\{1,2})runtime|core-js/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        loader: 'file-loader',
-        options: {
-          name: 'images/[name].[ext]?[hash]',
-        },
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'fonts/[name].[ext]?[hash]',
-            },
-          },
-        ],
-      },
-      // Non-standard assets on the dependency chain
-      {
-        test: /\.twig$/,
-        loader: 'file-loader',
-        options: {
-          emitFile: false,
-        },
-      },
-    ],
-  },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        sourceMap: NODE_ENV === 'production',
-      }),
-    ],
-  },
-  plugins: [
-    // Provides "global" vars mapped to an actual dependency. Allows e.g. jQuery
-    // plugins to assume that `window.jquery` is available
-    new ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery',
-    }),
-    // Handle .vue files
-    new VueLoaderPlugin(),
-    // Only add ProgressPlugin for non-production env.
-    ...(NODE_ENV === 'production'
-      ? []
-      : [new ProgressPlugin({ profile: false })]),
-  ],
-  resolve: {
-    alias: {
-      // Since we operate in a world where random Vue templates might have to
-      // be output via twig, we need the Vue build that includes the whole
-      // template compiling engine. If we are on a build that will NEVER read
-      // HTML from the DOM and use it as a template, then remove this line.
-      vue$: 'vue/dist/vue.esm.js',
+    // entry: {}, // See entryPrepend() and particle() below for entry details
+    mode: NODE_ENV, // development|production
+    output: {
+        filename: '[name].js',
     },
-  },
+    devtool: NODE_ENV === 'development' ? 'eval' : 'source-map',
+    module: {
+        rules: [{
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        js: 'babel-loader',
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: false,
+                        },
+                    },
+                    {
+                        loader: 'resolve-url-loader',
+                        options: {
+                            root: '',
+                            sourceMap: false,
+                        },
+                    },
+                    {
+                        // PostCSS config at ./postcss.config.js
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                ident: 'postcss',
+                            },
+                            sourceMap: false,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(js|vue)$/,
+                enforce: 'pre',
+                //exclude: /node_modules/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader',
+                options: {
+                    emitWarning: true, // development only
+                    // emitWarning: false, // production only
+                },
+            },
+            {
+                test: /\.js$/,
+                // @babel runtime and core must NOT be transformed by babel
+                exclude: /@babel(?:\/|\\{1,2})runtime|core-js/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'images/[name].[ext]?[hash]',
+                },
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: 'fonts/[name].[ext]?[hash]',
+                    },
+                }, ],
+            },
+            // Non-standard assets on the dependency chain
+            {
+                test: /\.twig$/,
+                loader: 'file-loader',
+                options: {
+                    emitFile: false,
+                },
+            },
+        ],
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                sourceMap: NODE_ENV === 'production',
+            }),
+        ],
+    },
+    plugins: [
+        // Provides "global" vars mapped to an actual dependency. Allows e.g. jQuery
+        // plugins to assume that `window.jquery` is available
+        new ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+        }),
+        // Handle .vue files
+        new VueLoaderPlugin(),
+        // Only add ProgressPlugin for non-production env.
+        ...(NODE_ENV === 'production' ? [] : [new ProgressPlugin({ profile: false })]),
+    ],
+    resolve: {
+        alias: {
+            // Since we operate in a world where random Vue templates might have to
+            // be output via twig, we need the Vue build that includes the whole
+            // template compiling engine. If we are on a build that will NEVER read
+            // HTML from the DOM and use it as a template, then remove this line.
+            vue$: 'vue/dist/vue.esm.js',
+        },
+    },
 };
