ca876e4f91d92afb1c121c6a0bb81df326877fd5
Lisa Ridley <l.ridley@vardot.com>
2024-07-26 19:24:45 +0000

    UTC-32: Remove molecules, organisms, templates and pages from source files, remove molecules and organisms from theme (not used)

--- source/default/jest.config.js
+++ source/default/jest.config.js
@@ -14,11 +14,7 @@ const dsPath = path.relative(path.join(__dirname, rootDir), __dirname);
  *   rootDir: '../../',
  *   moduleNameMapper: {
  *     '^protons[/]?(.*)': '<rootDir>/source/default/_patterns/00-protons/$1',
- *     '^atoms[/]?(.*)': '<rootDir>/source/default/_patterns/01-atoms/$1',
- *     '^molecules[/]?(.*)': '<rootDir>/source/default/_patterns/02-molecules/$1',
- *     '^organisms[/]?(.*)': '<rootDir>/source/default/_patterns/03-organisms/$1',
- *     '^templates[/]?(.*)': '<rootDir>/source/default/_patterns/04-templates/$1',
- *     '^pages[/]?(.*)': '<rootDir>/source/default/_patterns/05-pages/$1',
+ *     '^atoms[/]?(.*)': '<rootDir>/source/default/_patterns/01-atoms/$1'
  *   },
  *
  *   @TODO: Use Object.entries() when dropping support for Node 6
--- source/default/namespaces.js
+++ source/default/namespaces.js
@@ -16,9 +16,5 @@ module.exports = {
   tokens: path.resolve(__dirname, 'tokens'),
   // Atomic concepts
   protons: path.resolve(patterns, '00-protons'),
-  atoms: path.resolve(patterns, '01-atoms'),
-  molecules: path.resolve(patterns, '02-molecules'),
-  organisms: path.resolve(patterns, '03-organisms'),
-  templates: path.resolve(patterns, '04-templates'),
-  pages: path.resolve(patterns, '05-pages'),
+  atoms: path.resolve(patterns, '01-atoms')
 };
